a1c5776c41763e8fcd57e30da633359e9ebba5c5
Sunny-Baishnab <72726626+Sunny-Baishnab@users.noreply.github.com>
2021-04-29 19:27:06 +0530

    Add files via upload

---
+++ caseGraph.py
@@ -0,0 +1,7 @@
+import pandas as pd 
+import plotly.express as px
+
+df = pd.read_csv('Copy+of+data+-+data.csv')
+
+fig = px.scatter(df,x = 'date',y = 'cases', color = 'country', title = 'Covid of different Countries')
+fig.show()
